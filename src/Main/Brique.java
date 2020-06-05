a745e7b5237f2ea3a962a35d7dd23d457dfaf1f5
apresta <presta.alberto@libero.it>
2020-06-05 09:10:29 +0200

    Add AI_LOgicTest

--- src/Main/Brique.java
+++ src/Main/Brique.java
@@ -1,20 +1,11 @@
 import CLI.IO_Interface;
-import CLI.Launch_Game_cli;
 import Logic.Launch_Game;
 
 public class Brique {
 
     public static void main(String[] args) {
-        String inter_string = IO_Interface.ChooseInterface();
-        if (inter_string.equals("GUI")) {
-            Launch_Game game = new Launch_Game();
-            game.launch();
-        } else {
-            Launch_Game_cli game = new Launch_Game_cli();
-            game.launch();
-
-        }
+        boolean choose_interface = IO_Interface.ChooseInterface();
+        Launch_Game game = new Launch_Game(choose_interface);
+        game.launch();
     }
-
-
 }
